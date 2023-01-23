900cd26dbe049168c4aebb6c538410cc3d842a4c
varunsinghDG <85149973+varunsinghDG@users.noreply.github.com>
2023-01-23 17:06:48 +0530

    Account popover

--- src/layouts/dashboard/header/AccountPopover.js
+++ src/layouts/dashboard/header/AccountPopover.js
@@ -103,15 +103,15 @@ export default function AccountPopover() {
 
         <Divider sx={{ borderStyle: 'dashed' }} />
 
-        <Stack sx={{ p: 1 }}>
+        {/* <Stack sx={{ p: 1 }}>
           {OPTIONS.map((option) => (
             <MenuItem key={option.label} onClick={() => handleClickItem(option.linkTo)}>
               {option.label}
             </MenuItem>
           ))}
-        </Stack>
+        </Stack> */}
 
-        <Divider sx={{ borderStyle: 'dashed' }} />
+        {/* <Divider sx={{ borderStyle: 'dashed' }} /> */}
 
         <MenuItem onClick={handleLogout} sx={{ m: 1 }}>
           Logout
