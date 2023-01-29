8c3eda1a2cbdde6dbe6240493d96e7e94bc27127
varunsinghDG <85149973+varunsinghDG@users.noreply.github.com>
2023-01-29 16:32:11 +0530

    cors issue

--- src/functions/index.js
+++ src/functions/index.js
@@ -4,7 +4,11 @@ import axios from '../utils/axios';
 const accessToken = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : '';
 const config = {
     headers: {
-    Authorization : `Bearer ${accessToken}`
+        Authorization : `Bearer ${accessToken}`,
+       "Access-Control-Allow-Credentials": "true" ,
+       "Access-Control-Allow-Origin": "*" ,
+       "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT" ,
+       "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" 
     }
 };
 
