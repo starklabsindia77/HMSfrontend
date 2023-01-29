1dd038bcef8e8973c6112416002d67b114de9166
varunsinghDG <85149973+varunsinghDG@users.noreply.github.com>
2023-01-29 23:05:12 +0530

    few changes

--- next.config.js
+++ next.config.js
@@ -14,8 +14,9 @@ module.exports = withTM({
 
   env: {
     // HOST
-    HOST_API_KEY: 'https://hmsfrontend.vercel.app',
-    // HOST_API_KEY: 'http://localhost:3031',
+    HOST_API_KEY:'https://hmsfrontend.vercel.app',
+    //HOST_API_KEY: 'https://tripdeals.net',
+    //HOST_API_KEY: 'http://localhost:3031',
     // MAPBOX
     MAPBOX_API: '',
     MONGO_URI:"mongodb+srv://Admin:Noki@lumi@52@hms.89rftxr.mongodb.net/Travel?retryWrites=true&w=majority",
@@ -34,6 +35,20 @@ module.exports = withTM({
     // AUTH0
     AUTH0_DOMAIN: '',
     AUTH0_CLIENT_ID: '',
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   },
   async redirects() {
     return [
