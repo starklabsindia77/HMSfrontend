07382110cae700850195c885bdf4f62f9d9fdcfd
varunsinghDG <85149973+varunsinghDG@users.noreply.github.com>
2023-02-17 18:15:13 +0530

    Account Update

--- src/pages/dashboard/payment/index.js
+++ src/pages/dashboard/payment/index.js
@@ -13,6 +13,8 @@ import {
   Divider,
   TableBody,
   Container,
+  TextField,
+  Box,
   IconButton,
   TableContainer,
 } from '@mui/material';
@@ -24,40 +26,55 @@ import Scrollbar from '../../../components/scrollbar';
 import ConfirmDialog from '../../../components/confirm-dialog';
 import CustomBreadcrumbs from '../../../components/custom-breadcrumbs';
 import { useSettingsContext } from '../../../components/settings';
+import { RHFSelect, RHFTextField, RHFInput } from '../../../components/hook-form';
 // @mui
 import { useTheme } from '@mui/material/styles';
 // layouts
 import DashboardLayout from '../../../layouts/dashboard';
+import { AcceptHosted, FormComponent, FormContainer } from 'react-authorize-net';
 
-const authData = {
-  apiLoginID: '3VZ4jUAm36',
-  clientKey: '9bHPz94HT7ar45RZ',
-};
+// const authData = {
+//   apiLoginID: '3wqD5GMEy3xQ',
+//   clientKey: '6Cd376uSK958ZsaD',
+// };
 
 Index.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default function Index() {
-    const { push } = useRouter();
-    const { enqueueSnackbar } = useSnackbar();
-    const theme = useTheme();
+  const { push } = useRouter();
+  const { enqueueSnackbar } = useSnackbar();
+  const theme = useTheme();
+  const [amount, setAmount]= React.useState();
+
+  const clientKey =
+    "3f954d242gFcjc2yHRaQ4hbZ8YdmKtL7gWnTzFAdWLx8SbeHp3KejbFX927vgSFw";
+  const apiLoginId = "3wqD5GMEy3xQ";
+  const HandleAmount = (e) =>{
+    console.log("value", e.target.value)
+    setAmount(e.target.value);
+  }
 
   const { themeStretch } = useSettingsContext();
+  const onSuccessHandler = (response) => {
+    console.log("response", response);
+  };
+
+  const onErrorHandler = (error) => {
+    console.log("error", error);
+  };
+
   const handleSubmit = (response) => {
     console.log('Received response:', response);
-    if (response.messages.resultCode === "Error") {
+    if (response.messages.resultCode === 'Error') {
       var i = 0;
       while (i < response.messages.message.length) {
-          console.log(
-              response.messages.message[i].code + ": " +
-              response.messages.message[i].text
-          );
-          i = i + 1;
+        console.log(response.messages.message[i].code + ': ' + response.messages.message[i].text);
+        i = i + 1;
       }
     } else {
       enqueueSnackbar('Payment Done Successfully');
       push(PATH_DASHBOARD.general.app);
     }
-    
   };
   // return (
   //   <HostedForm authData={authData} onSubmit={handleSubmit} />
@@ -106,12 +123,31 @@ export default function Index() {
         </Card> */}
 
         <Card>
-          <HostedForm authData={authData} onSubmit={handleSubmit} environment={'SANDBOX'} />  
-          
+          <Box sx={{ mb: 5 , mt:5, justifyContent: 'center' }}>
+            <TextField
+                  // size="small"
+                  name={`Amount`}
+                  label="Amount"
+                  type="number"
+                  onChange={(e)=> HandleAmount(e)}
+                  InputLabelProps={{ shrink: true }}
+                />
+          </Box>
+
+        
+          <FormContainer
+            environment="sandbox"
+            onError={onErrorHandler}
+            onSuccess={handleSubmit}
+            amount={amount}
+            component={FormComponent}
+            clientKey={clientKey}
+            apiLoginId={apiLoginId}
+            
+          />
+          {/* <HostedForm authData={authData} onSubmit={handleSubmit} environment={'SANDBOX'} /> */}
         </Card>
       </Container>
-
-      
     </>
   );
-};
+}
